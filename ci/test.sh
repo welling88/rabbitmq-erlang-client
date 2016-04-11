8314e303d73cf8e211c00cc17b3f21a7581643d8
Brandon Shroyer <bshroyer@pivotal.io>
2016-04-11 19:07:40 -0400

    Remove apt-gets so we can try out docker image

--- ci/test.sh
+++ ci/test.sh
@@ -2,19 +2,6 @@
 
 set -ex
 
-apt-get update
-apt-get install -y --fix-missing --no-install-recommends \
-  bash \
-  debhelper \
-  git \
-  python-dev \
-  python-simplejson \
-  rsync \
-  xmlto \
-  xsltproc \
-  zip
-
-
 pushd rabbitmq-erlang-client
   make dep_rabbit_common="cp ../rabbit_common" tests
 popd
